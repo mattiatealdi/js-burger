434b800c5591aeeb263d5d2380160b6d37800791
mattiatealdi <mattiatealdi@gmail.com>
2021-04-16 18:09:24 +0200

    commit_first

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var nameUser = document.getElementById('name');
+var button = document.getElementById('button');
+
+/**
+ * settings
+ */
+ var defaultPrice = 50;
+ var coupons = ['sconto2021', 'sconto-bool'];
+ var discountValue = 0.3;
+
+ 
+
+
+
+
+function nameCheck(str){
+  var nomeInserito = str.value.trim();
+  if(nomeInserito.length === 0){
+    alert("Inserisci il nome del burger!")
+  }
+  else{
+    console.log("Il nome inserito è valido!");
+  }
+}
+
+button.addEventListener('click', function(){
+  nameCheck(nameUser)
+});
